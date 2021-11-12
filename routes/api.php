0f8a1dbc6e6a2744130050389633347a67715dc5
GlifosAFM <42324647+Glifos6089@users.noreply.github.com>
2021-11-12 17:54:59 -0500

    api prueba 2

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/handbags', 'App\Http\Controllers\Api\HandbagApi@index')->name("api.handbag.index");
+Route::get('/handbags', 'App\Http\Controllers\api\HandbagApi@index')->name("api.handbag.index");
