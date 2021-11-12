833b631349c71fef1e91a349cb289b9673167047
GlifosAFM <42324647+Glifos6089@users.noreply.github.com>
2021-11-12 17:48:47 -0500

    probando api

--- app/Http/Controllers/api/HandbagApi.php
+++ app/Http/Controllers/api/HandbagApi.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Resources\HandbagResource;
 use App\Http\Controllers\Controller;
 use App\Models\Handbag;
-use App\Models\Item;
 
 class HandbagApi extends Controller
 {
